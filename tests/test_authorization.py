e1c10af17167be194f151dd7ab377c643b41be33
matygullinruslan <matygullinruslan@gmail.com>
2024-02-24 21:33:23 +0300

    commit

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -3,22 +3,29 @@ import allure
 
 from selene import browser, have
 
+
 def test_authorization():
-    browser.open('/client_account/login')
-    browser.element('#email').type('rusel_21@mail.ru')
+    with allure.step('Открываем страницу регистрации'):
+        browser.open('/client_account/login')
+    with allure.step('Заполняем форму авторизации'):
+        browser.element('#email').type('rusel_21@mail.ru')
     browser.element('#password').type('qwerty123456')
-    browser.element('.co-button').click()
-    browser.element('[href="/client_account/contacts"]').click()
-    browser.element('#client_name').should(have.value('Руслан Матыгуллин'))
-    browser.element('#client_email').should(have.value('rusel_21@mail.ru'))
+    with allure.step('Отправляем данные'):
+        browser.element('.co-button').click()
+    with allure.step('Открываем страницу регистрации'):
+        browser.element('[href="/client_account/contacts"]').click()
+
+    with allure.step('Проверяем, что пользователь авторизован'):
+        browser.element('#client_name').should(have.value('Руслан Матыгуллин'))
+        browser.element('#client_email').should(have.value('rusel_21@mail.ru'))
 
 
-def test_authorization():
-    browser.open('/client_account/login')
-    browser.element('#email').type('rusel_21@mail.ru')
-    browser.element('#password').type('qwerty12345')
-    browser.element('.co-button').click()
-    browser.element('.co-notice--danger').should(have.text('Сочетание логина и пароля не подходит'))
+# def test_authorization():
+#     browser.open('/client_account/login')
+#     browser.element('#email').type('rusel_21@mail.ru')
+#     browser.element('#password').type('qwerty12345')
+#     browser.element('.co-button').click()
+#     browser.element('.co-notice--danger').should(have.text('Сочетание логина и пароля не подходит'))
 
 
 
