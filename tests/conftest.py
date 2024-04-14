8443ac79f46627f45436cd2a811c419a8780f20b
matygullinruslan <matygullinruslan@gmail.com>
2024-04-14 16:35:14 +0300

    commit1

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,6 +17,10 @@ def setup_browser(request):
     browser.config.base_url = 'https://boroda.land'
     browser.config.window_width = 1900
     browser.config.window_height = 1080
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = 'eager'
+    browser.config.driver_options = driver_options
+
 
     options = Options()
     selenoid_capabilities = {
