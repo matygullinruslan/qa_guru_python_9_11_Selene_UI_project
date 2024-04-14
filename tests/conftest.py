53626d4fc42c9c9e6a333eec8eb2737e6d83c7c9
matygullinruslan <matygullinruslan@gmail.com>
2024-04-14 14:06:16 +0300

    commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,9 +12,9 @@ def load_env():
     load_dotenv()
 
 
-login = os.getenv('user1')
-password = os.getenv('1234')
-remote_browser_url = os.getenv('selenoid.autotests.cloud/wd/hub')
+login = os.getenv('LOGIN')
+password = os.getenv('PASSWORD')
+remote_browser_url = os.getenv('REMOTE_BROWSER_URL')
 
 
 @pytest.fixture(scope='function', autouse=True)
